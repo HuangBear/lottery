13c6aa40d0ea1dac00db0418cc0f76bb4e7b9a7c
Gene Huang <huangbear0915@gmail.com>
2023-11-10 10:30:34 +0800

    feat: translate website title to EN

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css';
-import '/css/stypes.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import type { AppProps } from 'next/app';
-import Head from 'next/head';
+import "../styles/globals.css";
+import "/css/stypes.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Secure Vectors 抽獎系統</title>
+        <title>Secure Vectors Lucky Draw</title>
       </Head>
       <Component {...pageProps} />
     </>
