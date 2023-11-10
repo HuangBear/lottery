ec5aba8fb5b0c58fc52a57650c3cea7d7a440eb7
Gene Huang <huangbear0915@gmail.com>
2023-11-10 15:26:44 +0800

    chore: git demo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Secure Vectors Lucky Draw</title>
+        <title>Secure Vectorssss Lucky Draw</title>
       </Head>
       <Component {...pageProps} />
     </>
