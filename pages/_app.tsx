5c2421beaa5a0c086d854402afc13b815cbd7746
Gene Huang <huangbear0915@gmail.com>
2024-02-29 13:56:12 +0800

    chore: update dependencies

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,7 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
       <Head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Secure Vectorssss Lucky Draw</title>
+        <title>Secure Vectors Lucky Draw</title>
       </Head>
       <Component {...pageProps} />
     </>
